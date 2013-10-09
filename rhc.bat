b3c4b90ee38ab52314dd2dcdd7cafa102e496c7a
Romain <filirom1@gmail.com>
2013-10-09 13:29:31 +0200

    Fix when %LIBRA_SERVER_PROXY% is not defined

--- rhc.bat
+++ rhc.bat
@@ -2,8 +2,8 @@
 :: Use SETLOCAL so variables set in the script are not persisted
 @SETLOCAL
 
-set HTTP_PROXY="%LIBRA_SERVER_PROXY%"
-set HTTPS_PROXY="%LIBRA_SERVER_PROXY%"
+set HTTP_PROXY=%LIBRA_SERVER_PROXY%
+set HTTPS_PROXY=%LIBRA_SERVER_PROXY%
 
 :: Invoke 'rhc' (the calling script) as argument to ruby.
 :: Also forward all the arguments provided to it.
